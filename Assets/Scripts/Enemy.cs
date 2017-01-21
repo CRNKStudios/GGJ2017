4aa9a717e15b868867eca5773e568965df47cd4d
Spencer Pollock <srepollock@gmail.com>
2017-01-21 15:59:34 -0800

    :relieve: Enemy text UI added. Working on collision of notes, players and enemies. Then we will have a working alpha

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,23 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour 
 {
 	/// <summary>
-    /// Enemies cord to hit them with.
+    /// Text box to display the enemies chord
     /// </summary>
-	private Chord target;
+	public Text chordText;
 	/// <summary>
     /// Spawn of the cord shot from the enemy.
     /// </summary>
 	public GameObject chordSpawn, chordPrefab;
+	/// <summary>
+    /// This is the enemy cord size to damage them. Used for level difficulty in scaling. 1-3 only
+    /// </summary>
+	public int enemyDifficulty = 3;
+	/// <summary>
+    /// Shot diffculty used in level scaling.
+    /// </summary>
+	public int enemyShotDifficulty = 3;
+	/// <summary>
+    /// Enemies cord to hit them with.
+    /// </summary>
+	private Chord target;
+
+	void Awake()
+	{
+		shoot (enemyShotDifficulty);
+		setTarget();
+	}
 
 	// Use this for initialization
 	void Start () 
 	{
-		//target = new Chord ();
-		shoot (3);
+
 	}
 
 	// Update is called once per frame
@@ -26,16 +44,16 @@ public class Enemy : MonoBehaviour
 
 	}
 
-	//Use this to set enemy target note
 	/// <summary>
     /// The enemies cord to hit them with.
     /// </summary>
     /// <param name="_n">Array of notes</param>
-	public void setTarget (Notes[] _n)
+	public void setTarget ()
 	{
-		target.Initialize (_n, Direction.None);
+		target = new Chord();
+		target.Initialize(Direction.None, enemyDifficulty);
+		setTextBar(target.getNotes());
 	}
-
 	/// <summary>
     /// Spawn random cords with given size notes.
     /// </summary>
@@ -44,5 +62,18 @@ public class Enemy : MonoBehaviour
 	{
 		GameObject bullet = (GameObject)Instantiate(chordPrefab, chordSpawn.transform.position, new Quaternion());
 		bullet.GetComponent<Chord>().randomChord (size);
+	}
+	/// <summary>
+    /// Sets the text bar with the notes of the object
+    /// </summary>
+    /// <param name="_n">Notes of the player</param>
+	private void setTextBar(Notes[] _n)
+	{
+		string o = "";
+		for (int i = 0; i < _n.Length; i++)
+		{
+			o += _n[i].ToString() + " ";
+		}
+		chordText.text = o;
 	}
 }
